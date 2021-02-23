4e345a68701ea07ece330c0141726f4c89154982
Anne-Sophie Cusson <64850625+4nn3-50ph13@users.noreply.github.com>
2021-02-22 20:21:18 -0500

    Ajoutez des fichiers projet.

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("slam")]
+        public async Task slam()
+        {
+            await ReplyAsync(":dog: Slam the Ma'am! :dog:");
+        }
+
+        [Command("SayHi")]
+        public async Task SayHi()
+        {
+            await ReplyAsync("Woof :loudspeaker:");
+        }
+
+        [Command("WhoAmI")]
+        public async Task WhoAmI(ushort size = 512)
+        {
+            await ReplyAsync(CDN.GetUserAvatarUrl(Context.User.Id, Context.User.AvatarId, size, ImageFormat.Auto));
+        }
+    }
+}
